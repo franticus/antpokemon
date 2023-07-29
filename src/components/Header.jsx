220b9f11639f6929cbd57620dfa9590087633b4b
S.Zelensky <zelenstein@gmail.com>
2023-07-29 14:13:17 +0300

    pro-layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageHeader } from '@ant-design/pro-layout';
+
+const Header = () => {
+  return (
+    <PageHeader
+      title='Ant Desing Pokemon'
+    />
+  );
+};
+
+export default Header;
